69ac1e44b1a3a1b47edd9277d7cfc1f7d9fdb14b
AmitGurung06 <amit.gurung@ada.ac.uk>
2025-02-03 20:12:03 +0000

    1

--- queries/request-1.sql
+++ queries/request-1.sql
@@ -2,6 +2,29 @@
 .mode column
 
 -- 1.1
+SELECT * FROM staff;
+
+SELECT staffno, fname, lname, salary 
+FROM staff;
+
+SELECT staffno, fname, lname, position 
+FROM staff
+WHERE position = 'Manager' AND position = 'Supervisor';
+
+SELECT ownerno, fname, lname
+FROM privateowner
+WHERE address LIKE '%Glasgsow%';
+
+SELECT view_id, clientno, viewdate
+FROM viewing 
+WHERE propertyno = 'PR4567' AND comment IS NULL; 
+
+SELECT staffno, fname, lname
+FROM staff  
+WHERE salary BETWEEN 20000 AND 30000;
+
+
+
 
 
 
--- queries/request-2.sql
+++ queries/request-2.sql
@@ -2,6 +2,68 @@
 .mode column
 
 -- 2.1
+SELECT count(*)
+FROM propertyforrent 
+WHERE rent > 350;
+
+SELECT count(*)
+FROM viewing 
+WHERE viewdate BETWEEN 2024-05-01 AND 2024-05-31;
+
+SELECT count(*), SUM (salary)
+FROM staff
+WHERE position = 'Manager';
+
+SELECT AVG(salary) AS average, MIN(salary) as minimum, MAX( salary) as maximum 
+FROM staff;
+
+SELECT branchno,count(*) AS totalStaff, SUM(salary) as totalSalary
+FROM staff
+GROUP By branchno; 
+
+SELECT branchno AS branch ,SUM(salary) as totalSalary, COUNT(staffno) as totalStaff
+FROM staff
+WHERE branchno > 1 
+GROUP BY branchno; 
+
+SELECT staffno, fname, lname
+FROM staff 
+WHERE branchno = 'B052';
+
+
+
+
+
+SELECT propertyno,street , city, postcode, type, rent FROM propertyforrent WHERE staffno in
+ (SELECT staffno FROM staff WHERE branchno = 
+ (SELECT branchno FROM branch WHERE street = '163 MAin St'));
+
+
+
+SELECT staffno, fname, name, position, salary
+FROM staff 
+WHERE salary > SOME (SELECT salary FROM staff WHERE branchno = ‘B003’
+
+
+
+
+
+
+SELECT *
+FROM staff 
+WHERE salary > (
+    SELECT MAX(salary) 
+    FROM staff 
+    WHERE branchNo = 'B052'
+);
+
+
+
+SELECT staffno, fname, name, position, salary
+FROM staff 
+WHERE salary > SOME (SELECT salary FROM staff WHERE branchno ='B003')
+
+
 
 
 
